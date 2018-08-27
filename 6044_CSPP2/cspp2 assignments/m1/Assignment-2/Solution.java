c84898e7a10bbf0db6ff64b3ae63ba1313a08f5a
sreegayathri <sree0291@msitprogram.net>
2018-08-27 19:47:54 +0530

    2nd a2

---
+++ 6044_CSPP2/cspp2 assignments/m1/Assignment-2/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int base = scan.nextInt();
+		int height = scan.nextInt();
+		float areaOfTriangle = 0.5f*base*height;
+		System.out.println(areaOfTriangle);
+	}
+	/*
+	Need to fill the areaOfTriangle function and print the output
+	of fahrenheit.
+	*/
+}
