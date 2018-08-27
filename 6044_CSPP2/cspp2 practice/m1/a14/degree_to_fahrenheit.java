5e27d22ce7460698d539325a137c6575342c78cd
sreegayathri <sree0291@msitprogram.net>
2018-08-27 21:26:25 +0530

    a14 f to d

---
+++ 6044_CSPP2/cspp2 practice/m1/a14/degree_to_fahrenheit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class convert {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		double farenheit = scan.nextDouble();
+		double cdegree = farenheit * 1.8 + 32;
+		System.out.println(cdegree);
+	}
+	/*
+	Need to fill the calculateFromDegreesToFarenheit function and print the output
+	of fahrenheit.
+	*/
+}
