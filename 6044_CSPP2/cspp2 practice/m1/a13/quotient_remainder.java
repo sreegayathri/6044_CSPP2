81761170489b0bb1516f3184210a6dd8acdd5369
sreegayathri <sree0291@msitprogram.net>
2018-08-27 21:43:24 +0530

    q and r A13

---
+++ 6044_CSPP2/cspp2 practice/m1/a13/quotient_remainder.java
@@ -0,0 +1,13 @@
+class QuotientRemainder {
+
+    public static void main(String[] args) {
+
+        int dividend = 36, divisor = 5;
+
+        int q = dividend / divisor;
+        int r = dividend % divisor;
+
+        System.out.println("Quotient = " + q);
+        System.out.println("Remainder = " + r);
+    }
+}
