82f37aa0e65ffb38f8f7da191e5ee3dfd6325fa9
sreegayathri <sree0291@msitprogram.net>
2018-08-27 19:58:12 +0530

    m1 c to f a4

---
+++ 6044_CSPP2/cspp2 assignments/m1/Assignment-3/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double degreesCelsius = scan.nextDouble();
+		double farenheit = degreesCelsius * 1.8 + 32;
+		System.out.println(farenheit);
+	}
+	/*
+	Need to fill the calculateFromDegreesToFarenheit function and print the output
+	of fahrenheit.
+	*/
+}
