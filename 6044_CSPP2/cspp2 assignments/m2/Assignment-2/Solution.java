862d789d1774ba2547daf08a83e425d3caefb705
sreegayathri <sree0291@msitprogram.net>
2018-08-28 17:41:11 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ 6044_CSPP2/cspp2 assignments/m2/Assignment-2/Solution.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+/**.
+*program of quadratic equation
+*/
+public final class Solution {
+    /**.
+    class of solution.
+    */
+    private Solution() {
+        /**.
+        this is a constructor*/
+    }
+    /**.
+    *@param args the arguments */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        quadratic(a, b, c);
+    }
+    /**. solving the quadratic equation
+    * @param a = numeric element
+    * @param b = numeric element
+    * @param c = numeric element*/
+    public static void quadratic(final int a, final int b, final int c) {
+        /**. conditions for quadratic equation*/
+        final int value = 4;
+        double root = Math.sqrt((b * b) - (value * a * c));
+        double numerator1 = -b + root;
+        double numerator2 = -b - root;
+        double denominator = 2 * a;
+        double value1 = numerator1 / denominator;
+        double value2 = numerator2 / denominator;
+        System.out.println(value1 + " " + value2);
+    }
+    /*
+    Need to write the rootsOfQuadraticEquation function and print the output.
+    */
+}
