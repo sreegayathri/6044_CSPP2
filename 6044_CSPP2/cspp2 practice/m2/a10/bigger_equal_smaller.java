1e88f24bd01562ff458004e7617cfe3d3998ba94
sreegayathri <sree0291@msitprogram.net>
2018-08-28 19:24:05 +0530

    comapring of two numbers

---
+++ 6044_CSPP2/cspp2 practice/m2/a10/bigger_equal_smaller.java
@@ -0,0 +1,19 @@
+public class bigger_equal_smaller {
+ 
+	public static void main(String[] args) {
+		
+		
+		int VarA = 1234;
+		int VarB = 2345;
+		
+		if(VarA > VarB){
+			System.out.println("Bigger");
+		}
+		else if(VarA < VarB){
+			System.out.println("Smaller");
+		}
+		else{
+			System.out.println("Equal");
+		}
+	}
+}
