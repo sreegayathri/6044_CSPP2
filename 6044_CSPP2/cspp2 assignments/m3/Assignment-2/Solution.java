26048f846ceb7b694aa754c3a366918a8858d8ed
sreegayathri <sree0291@msitprogram.net>
2018-08-29 21:27:17 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- 6044_CSPP2/cspp2 assignments/m3/Assignment-2/Solution.java
+++ 6044_CSPP2/cspp2 assignments/m3/Assignment-2/Solution.java
@@ -2,47 +2,28 @@ import java.util.Scanner;
 /*
     Do not modify this main function.
     */
-/**
- * { item_description }.
- */
-final class Solution {
-    /**
-     * Constructs the object.
-     */
-
+public final class Solution {
     private Solution() {
-        //Nothing happens here
+        /**.
+        * constructor of the class*/
     }
-    /**
-     * { function_description }.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        /**
-         * { var_description }.
-         */
-
-        Scanner s = new Scanner(System.in);
-        int n1 = s.nextInt();
-        int n2 = s.nextInt();
-        System.out.println(gcd(n1, n2));
-    }
-    /**
-     * { function_description }.
-     *
-     * @param      n1    1st number
-     * @param      n2    2nd number
-     *
-     * @return     { description_of_the_return_value }
-     */
-    public static int gcd(final int n1, final int n2) {
-        int greaterdivisor = 1;
-        for (int i = 2; i <= n2; i++) {
-            if (n1 % i == 0 && n2 % i == 0) {
-                greaterdivisor = i;
+/* Fill the main function to print the number of 7's between 1 to n*/
+    public static void main(String[] args) {
+        /**.
+        * main function of the program */
+        Scanner s=new Scanner(System.in);      
+        int n = s.nextInt();
+        int count = 0;
+        for (int i=1;i<=n;i++) {
+            int temp = i;
+            while (temp!=0){
+                if (temp%10 == 7) {
+                    count = count +1;
+                }
+                temp=temp/10;
             }
         }
-        return greaterdivisor;
+        System.out.println(count);
+
     }
 }
