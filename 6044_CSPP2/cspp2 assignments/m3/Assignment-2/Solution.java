7632fe5398035d57fc39541005f84dee417e6e3c
sreegayathri <sree0291@msitprogram.net>
2018-08-29 21:43:23 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- 6044_CSPP2/cspp2 assignments/m3/Assignment-2/Solution.java
+++ 6044_CSPP2/cspp2 assignments/m3/Assignment-2/Solution.java
@@ -2,29 +2,32 @@ import java.util.Scanner;
 /*
     Do not modify this main function.
     */
+/**
+ * Class for solution.
+ */
 public final class Solution {
-    /**.
-    * class of the program*/
+    /**
+     * Constructs the object.
+     */
     private Solution() {
         /**.
-        * constructor of the class*/
+        * constructor of class*/
     }
 /* Fill the main function to print the number of 7's between 1 to n*/
-    /**.
+    /**
     * @param n the given n numbers
     * @param i initialization
     * @param count number count of total 7s
     * @param temp temporary variable*/
     public static void main(final String[] args) {
-        /**.
-        * main function of the program*/
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int count = 0;
+        int i;
         int temp = 1;
         final int x = 10;
         final int y = 7;
-        for (int i = 0; i <= n; i++) {
+        for (i = 0; i <= n; i++) {
             temp = i;
             while (temp != 0) {
                 if (temp % x == y) {
