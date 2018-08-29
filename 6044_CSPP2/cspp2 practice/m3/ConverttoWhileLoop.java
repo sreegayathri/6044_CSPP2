828d7bf184558ee1f0edbfb7b6c62b6bfb1bd5b5
sreegayathri <sree0291@msitprogram.net>
2018-08-29 21:02:08 +0530

    ConverttoWhileLoop

---
+++ 6044_CSPP2/cspp2 practice/m3/ConverttoWhileLoop.java
@@ -0,0 +1,10 @@
+class ConverttoWhileLoop {
+	public static void main(String[] args) {
+		int i = 2;
+		while (i<=10){
+			System.out.println(i);
+			i = i + 2;
+		}
+		System.out.println("GoodBye1");
+	}
+}
