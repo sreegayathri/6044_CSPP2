485d511b8d262b2b1be7ff3a282530bb0212a58d
sreegayathri <sree0291@msitprogram.net>
2018-08-29 21:16:48 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ 6044_CSPP2/cspp2 assignments/m3/Assignment-3/Solution.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+/*
+    Do not modify this main function.
+    */
+/**
+ * class begins.
+ */
+final class Solution {
+    /**
+     * the class.
+     */
+
+    private Solution() {
+    }
+    /**
+     * Constructor of class.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * main function of program.
+         */
+
+        Scanner s = new Scanner(System.in);
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+        System.out.println(gcd(n1, n2));
+    }
+    /**
+     * params of gcd function.
+     *
+     * @param      n1    1st number
+     * @param      n2    2nd number
+     *
+     * @return     greater common factor
+     */
+    public static int gcd(final int n1, final int n2) {
+        int greaterfactor = 1;
+        for (int i = 2; i <= n2; i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                greaterfactor = i;
+            }
+        }
+        return greaterfactor;
+    }
+}
