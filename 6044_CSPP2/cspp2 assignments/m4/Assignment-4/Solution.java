16e1c1e04cab50c8e9c0a780d76861af01fbe378
sreegayathri <sree0291@msitprogram.net>
2018-08-31 19:03:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- 6044_CSPP2/cspp2 assignments/m4/Assignment-4/Solution.java
+++ 6044_CSPP2/cspp2 assignments/m4/Assignment-4/Solution.java
@@ -1,17 +1,15 @@
 /**. Given an String, Write a java method that
  *returns the decimal value for the given binary string.*/
 import java.util.Scanner;
-/**
+/**.
  * Class for solution.
  */
 public final class Solution {
-    /**
+    /**.
      * { function_description }
-     *
-     * @param      args  The arguments
      */
     private Solution() {
-        /**
+        /**.
          * { item_description }
          */
     }
