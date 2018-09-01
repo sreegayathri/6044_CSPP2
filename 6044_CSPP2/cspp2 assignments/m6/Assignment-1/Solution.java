2c1eccf57be85a55c446341f19a5eda2d1bedf27
sreegayathri <sree0291@msitprogram.net>
2018-09-01 13:16:48 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

--- 6044_CSPP2/cspp2 assignments/m6/Assignment-1/Solution.java
+++ 6044_CSPP2/cspp2 assignments/m6/Assignment-1/Solution.java
@@ -1,34 +1,53 @@
 import java.util.Scanner;
-
-public class Solution {
-
-    private static String comps;
-    public static void main(String[] args){
-
-        Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-        comps = "";
-        boolean prime = isPrime(number);
-        if(prime){
-            System.out.println(number+" is Prime");
-        }
-        else{
-            System.out.println("The number "+number+" is composed of "+comps);
-        }
+/**
+ * Write a java program to find the odd composite ns
+ * between 2 and n where n is given as an scan.
+ *
+ * @author : 
+ */
+public final class Solution {
+    /**
+    * Empty constructor.
+    */
+    private Solution() {
+        //not used
     }
-    public static boolean isPrime(int num){
+    private static String comps;
+    /**
+     * Prints the odd composite ns between 2 and n.
+     *
+     * @param      n     n value
+     */
+    public static boolean oddComposites(int n){
         boolean isTrue = true;
-        for(int i=2; i<=Math.ceil((num/2)); i++){
-            if(num%i == 0){
+        for(int i = 2; i <= Math.ceil((n / 2)); i++){
+            if(n % i == 0){
                 if(comps.equals("")){
                     comps += ""+i;
                 }
                 else{
-                    comps += ","+i;
+                    comps += "," + i;
                 }
                 isTrue = false;
             }
         }
         return isTrue;
+    }
+    /**
+    * main method as driver program.
+    * @param args is the parameter for this method
+    */
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int nber = scan.nextInt();
+        comps = "";
+        boolean prime = oddComposites(nber);
+        if(prime){
+            System.out.println(nber+" is Prime");
+        }
+        else{
+            System.out.println(nber);
+        }
     }
 }
