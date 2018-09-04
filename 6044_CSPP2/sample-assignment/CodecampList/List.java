a6e26eb3e94231eb3eb59956feb73847e8555e6e
sreegayathri <sree0291@msitprogram.net>
2018-09-04 15:59:50 +0530

    "check_style_score -> 1 of 2 passed. style score: 0/1 "

--- 6044_CSPP2/sample-assignment/CodecampList/List.java
+++ 6044_CSPP2/sample-assignment/CodecampList/List.java
@@ -253,6 +253,7 @@ public class List {
                 case "contains":
                 System.out.println(l.contains(Integer.parseInt(tokens[1])));
                 break;
+                default:
             }
         }
     }
