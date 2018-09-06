59c2d99baf0e609abc1dfbb37a51091402a756a4
sreegayathri <sree0291@msitprogram.net>
2018-09-06 18:51:28 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- 6044_CSPP2/cspp2 assignments/m10/Assignment-1/List.java
+++ 6044_CSPP2/cspp2 assignments/m10/Assignment-1/List.java
@@ -277,7 +277,7 @@ public class List {
         }
         str = str + list[i] + "]";
         if (list[0] == 0) {
-            str += "\n Invalid Position Exception";
+            str += "\nInvalid Position Exception";
         }
         return str;
     }
