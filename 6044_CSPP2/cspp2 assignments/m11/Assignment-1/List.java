9f7f3623118843f6909eab890a122d8609877957
sreegayathri <sree0291@msitprogram.net>
2018-09-07 15:45:24 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- 6044_CSPP2/cspp2 assignments/m11/Assignment-1/List.java
+++ 6044_CSPP2/cspp2 assignments/m11/Assignment-1/List.java
@@ -384,18 +384,16 @@ public class List {
 					l.removeAll(a);
 				}
 				break;
-			case "subList": {
+			case "subList":
 				if (tokens.length != 2) {
 					break;
 				}
 				String[] arrstring3 = tokens[1].split(",");
-				List object = l.subList(Integer.parseInt
-					(arrstring3[0]), Integer.parseInt(arrstring3[1]));
+				List object = l.subList(Integer.parseInt(arrstring3[0]),Integer.parseInt(arrstring3[1]));
 				if (object != null) {
 					System.out.println(object);
 				}
 				break;
-			}
 			case "equals":
 				if (tokens.length == 2) {
 					String[] lt = tokens[1].split(",");
