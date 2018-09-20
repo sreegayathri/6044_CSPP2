cabebd7806b48f3c2af20705a85fe33e1268dc91
sreegayathri <sree0291@msitprogram.net>
2018-09-20 23:52:03 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- 6044_CSPP2/cspp2 assignments/m23/Document.java
+++ 6044_CSPP2/cspp2 assignments/m23/Document.java
@@ -1,19 +1,19 @@
 import java.util.*;
 class Document {
 
-	private String filename;
-	private HashMap<String, Integer> hashmap;
+    private String filename;
+    private HashMap<String, Integer> hashmap;
 
-	Document(String filename, HashMap<String, Integer> hashmap) {
-		this.filename = filename;
-		this.hashmap = hashmap;
-	}
+    Document(String filename, HashMap<String, Integer> hashmap) {
+        this.filename = filename;
+        this.hashmap = hashmap;
+    }
 
-	public String getfilename() {
-		return filename;
-	}
+    public String getfilename() {
+        return filename;
+    }
 
-	public HashMap<String, Integer> getHashMap() {
-		return hashmap;
-	}
+    public HashMap<String, Integer> getHashMap() {
+        return hashmap;
+    }
 }
