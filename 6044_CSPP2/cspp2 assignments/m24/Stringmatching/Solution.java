26383d41879990eaac9782482a9998f65cf2f6c7
sreegayathri <sree0291@msitprogram.net>
2018-09-23 19:26:06 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- 6044_CSPP2/cspp2 assignments/m24/Stringmatching/Solution.java
+++ 6044_CSPP2/cspp2 assignments/m24/Stringmatching/Solution.java
@@ -1,51 +1,128 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 import java.io.FileReader;
-public class Solution {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
 
-	public static String fileRead(final String folder, final String name)
-        throws Exception {
-        BufferedReader br = new BufferedReader(
-            new FileReader("G:\\cspp2\\6044_CSPP2\\cspp2 assignments\\m24\\Stringmatching"
-                + folder + "\\" + name));
-        String content = "";
-        String line;
-        try {
-            while ((line = br.readLine()) != null) {
-                content += line;
-            }
-        } catch (Exception e) {
-                e.printStackTrace();
-        } finally {
-            try {
-                br.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return content;
+/**this class is to maintain.
+*complete details of two files.
+*/
+class Data {
+    /** this is an empty constructor.
+    */
+    Data() {
     }
-	public static void main(String[] args) throws Exception {
-		Similarity sim = new Similarity();
-        Scanner s = new Scanner(System.in);
-        if (!(s.hasNext())) {
-            System.out.println("empty directory");
-            return;
+    /**this method is to convert the.
+    *file document text to string
+    *@param file File
+    *@return str returns string of that text.
+    */
+    public static String toText(final File file) {
+        String str = "";
+        try {
+            Scanner input = new Scanner(new FileReader(file));
+            StringBuilder text = new StringBuilder();
+            while (input.hasNext()) {
+                text.append(input.next());
+                text.append(" ");
+            }
+            input.close();
+            str = text.toString();
+        } catch (FileNotFoundException e) {
+            System.out.println("No file");
         }
-        String folder = s.nextLine();
-        File file = new File("G:\\cspp2\\6044_CSPP2\\cspp2 assignments\\m24\\Stringmatching" + folder);
-        String[] str = file.list();
-        // System.out.println(Arrays.toString(str));
-        for (String files : str) {
-        	// System.out.println(files);
-            String read = fileRead(folder, files);
-            String[] content = read.split(" ");
-            Document d = new Document(files, content, read.length());
-            // System.out.println(read);
-            // System.out.println(Arrays.toString(content));
-            // System.out.println(read.length());
-            sim.addDocument(d);
+        return str;
+    }
+    /**this method is to give the.
+     *document distance.
+     *@param textOne first file string
+     *@param textTwo second file string
+     *@return document distance
+     */
+
+    public double stringMatching(final String textOne, final String textTwo) {
+        int lengthOne = textOne.length();
+        int lengthTwo = textTwo.length();
+        double totalLength = lengthOne + lengthTwo;
+        int max = 0;
+        double lcs = 0;
+        final int hundred = 100;
+        int[][] array = new int[lengthOne][lengthTwo];
+        for (int i = 0; i < lengthOne; i++) {
+            for (int j = 0; j < lengthTwo; j++) {
+                if (textOne.charAt(i) == textTwo.charAt(j)) {
+                    if (i == 0 || j == 0) {
+                        array[i][j] = 1;
+                    } else {
+                        array[i][j] = array[i - 1][j - 1] + 1;
+                    }
+                    if (max < array[i][j]) {
+                        max = array[i][j];
+                    }
+                }
+            }
         }
-        sim.getDocumentSimilarity();
+        lcs = (((max * 2) / totalLength) * hundred);
+        return lcs;
+    }
+}
+/** this is the solution class.
+*/
+public final class Solution {
+    /** an empty constructor.
+    */
+    private Solution() {
+
+    }
+    /**
+     * this is main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        try  {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        File files = new File(input);
+        Data obj = new Data();
+        File[] fileList = files.listFiles();
+        int length = fileList.length;
+        double maxValue = 0;
+        final int hundred = 100;
+        String result = "";
+        double[][] fileMatrix = new double[length][length];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (i == j) {
+                    fileMatrix[i][j] = hundred;
+                } else {
+                    fileMatrix[i][j] = obj.stringMatching(
+                        obj.toText(fileList[i]), obj.toText(fileList[j]));
+                    if (maxValue < fileMatrix[i][j]) {
+                        maxValue = fileMatrix[i][j];
+                        result = "Maximum similarity is between "
+                        + fileList[i].getName() + " and "
+                        + fileList[j].getName();
+                    }
+                }
+            }
+        }
+        System.out.print("      \t");
+        for (int i = 0; i < length - 1; i++) {
+            System.out.print("\t" + fileList[i].getName());
+        }
+        System.out.println("\t" + fileList[length - 1].getName());
+        for (int i = 0; i < length; i++) {
+            System.out.print(fileList[i].getName() + "\t");
+            for (int j = 0; j < length; j++) {
+                    System.out.print(
+                        String.format("%.1f", fileMatrix[i][j]) + "\t\t");
+            }
+            System.out.println();
+        }
+     System.out.println(result);
+    } catch (NoSuchElementException e) {
+        System.out.println("Empty Directory");
+    }
     }
 }
